686c08146d471f3fcbbe8c6a669d3f30610c9173
Cole <cole.vita@unc.edu>
2023-11-26 13:46:44 -0500

    Add check for session data with timesteps per frame
    - Create function check_file_exists to see if given session data with timesteps
    per frame exists before processing
    - Update master function to include check

--- ProcessData/load_processed_data.py
+++ ProcessData/load_processed_data.py
@@ -2,6 +2,21 @@ import os
 import pickle
 import pandas as pd
 
+def check_file_exists(session_id, output_dir="output", file_prefix="filtered_normalized_pickle", timesteps_per_frame=10):
+    """
+    Check if the pickle file exists in the specified directory.
+    
+    Parameters:
+    session_id (int): The ID of the session.
+    output_dir (str): The directory where the pickle file is expected to be found.
+    file_prefix (str): The prefix of the pickle file.
+    
+    Returns:
+    bool: True if the file exists, False otherwise.
+    """
+    pickle_path = os.path.join(output_dir, f"{file_prefix}_{session_id}_{timesteps_per_frame}.pkl")
+    return os.path.exists(pickle_path)
+
 def load_pickle_file(pickle_path):
     """
     Load a pickle file from the given path.
@@ -52,9 +67,9 @@ def clean_avg_firing_rates(df, highest_value, lowest_value):
     
     return filtered_normalized_firing_rates
 
-def master_cleaning_and_saving(session_id, output_dir='output', original_pickle_prefix='filtered_normalized_pickle', new_pickle_prefix='spike_trains_with_stimulus_session', highest_value=100, lowest_value=0):
+def master_cleaning_and_saving(session_id, output_dir='output', timesteps_per_frame=10, original_pickle_prefix='filtered_normalized_pickle', new_pickle_prefix='spike_trains_with_stimulus_session', highest_value=100, lowest_value=0):
     """
-    Master function to clean and save the firing rate data within an 'output' directory.
+    Master function to check, clean, and save the firing rate data within an 'output' directory.
     
     Parameters:
     session_id (int): The ID of the session to be processed.
@@ -64,24 +79,32 @@ def master_cleaning_and_saving(session_id, output_dir='output', original_pickle_
     highest_value (float): The threshold to remove neurons with a high z-score.
     lowest_value (float): The threshold to remove neurons with a z-score never exceeding this value.
     """
-    # Construct the paths to the original and new pickle files
-    original_pickle_path = os.path.join(output_dir, f'{original_pickle_prefix}_{session_id}.pkl')
-    new_pickle_path = os.path.join(output_dir, f'{new_pickle_prefix}_{session_id}.pkl')
+    # Check if the original pickle file exists
+    if check_file_exists(session_id, output_dir, original_pickle_prefix, timesteps_per_frame):
+        # Construct the path to the original pickle file
+        original_pickle_path = os.path.join(output_dir, f'{original_pickle_prefix}_{session_id}_{timesteps_per_frame}.pkl')
 
-    # Load the original data
-    spike_df = load_pickle_file(original_pickle_path)
-    
-    if spike_df is not None:
+        # Load the original data
+        spike_df = load_pickle_file(original_pickle_path)
+        
         # Clean the average firing rates
         filtered_normalized_firing_rates = clean_avg_firing_rates(spike_df, highest_value, lowest_value)
 
         # Drop rows where the frame is -1
         filtered_normalized_firing_rates = filtered_normalized_firing_rates[filtered_normalized_firing_rates['frame'] != -1]
 
+        # Construct the path to the new pickle file
+        new_pickle_path = os.path.join(output_dir, f'{new_pickle_prefix}_{session_id}.pkl')
+
         # Save the cleaned data
         save_pickle_file(filtered_normalized_firing_rates, new_pickle_path)
+        print(f"Original data file for session {session_id} has been cleaned and saved.")
+        
     else:
-        print(f"Data for session {session_id} was not found or could not be loaded.")
+        print(f"Original data file for session {session_id} does not exist in '{output_dir}' directory.")
+
+    return filtered_normalized_firing_rates
 
 # Example usage:
 # master_cleaning_and_saving(session_id=721123822)
+
